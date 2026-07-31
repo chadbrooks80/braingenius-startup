1da7eea6d144f5856607333f65dd65da6dbf1ea9
Chad <chadbrooks80@gmail.com>
2026-07-31 08:05:09 -0500

    created temp link to go to vocab learning module

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 const DashboardPage = () => {
-    return ( <div>hello</div> );
+    return ( <div><a href="https://www.braingenius.ai/learning/vocabulary/cms7woxm80000dfsbg5cjcknj">click me</a></div> );
 }
  
 export default DashboardPage;
